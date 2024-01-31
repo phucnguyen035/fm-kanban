3cab62173bd1cb7885f3b14f575ed5534f6c0b9e
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2024-01-31 13:09:29 +0700

    Add TaskCard component

---
+++ app/components/TaskCard.tsx
@@ -0,0 +1,40 @@
+import { css } from "styled-system/css";
+import { vstack } from "styled-system/patterns";
+import type { TaskStatus } from "~/.server/data-access/task/schema";
+
+type Props = {
+  title: string;
+  subTasks: Array<{ status: TaskStatus }>;
+};
+
+export default function TaskCard({ title, subTasks }: Props) {
+  const doneSubTasks = subTasks.filter((subTask) => subTask.status === "done");
+
+  return (
+    <article
+      className={css({
+        width: 280,
+        paddingBlock: 6,
+        paddingInline: 4,
+        bgColor: { base: "white", _dark: "grey.dark" },
+        borderRadius: "lg",
+        boxShadow: "0px 4px 6px 0px rgba(54, 78, 12y, 0.10)",
+      })}
+    >
+      <button
+        className={vstack({
+          alignItems: "flex-start",
+          gap: 2,
+          textAlign: "left",
+        })}
+      >
+        <h3 className={css({ textStyle: "headingM" })}>{title}</h3>
+        {subTasks.length > 0 && (
+          <p className={css({ textStyle: "bodyM", color: "grey.medium" })}>
+            {doneSubTasks.length} of {subTasks.length} subtasks
+          </p>
+        )}
+      </button>
+    </article>
+  );
+}
