3e45e6bfdd5df4fdc5b674c8141523d91301f1ba
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2024-02-10 18:37:47 +0700

    Add Button component

---
+++ app/components/Button.tsx
@@ -0,0 +1,68 @@
+import { cva } from "styled-system/css";
+import { styled } from "styled-system/jsx";
+
+export const buttonStyle = cva({
+  base: {
+    display: "inline-flex",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "3xl",
+    cursor: {
+      base: "pointer",
+      _disabled: "not-allowed",
+    },
+    px: "18px",
+    transition: "colors",
+  },
+  variants: {
+    size: {
+      large: {
+        height: 12,
+        textStyle: "headingM",
+      },
+      small: {
+        height: 10,
+        fontSize: "13px",
+        fontWeight: 700,
+        lineHeight: "23px",
+      },
+    },
+    variant: {
+      primary: {
+        color: "white",
+        bgColor: {
+          base: "purple.base",
+          _hover: "purple.light",
+          _disabled: "purple.light",
+        },
+      },
+      secondary: {
+        color: "purple.base",
+        bgct: {
+          base: "purple.base/90",
+          _hover: "purple.base/75",
+          _dark: {
+            base: "white",
+            _hover: "white",
+          },
+        },
+      },
+      destructive: {
+        color: "white",
+        bgColor: {
+          base: "red.base",
+          _hover: "red.light",
+          _disabled: "red.light",
+        },
+      },
+    },
+  },
+  defaultVariants: {
+    size: "large",
+    variant: "primary",
+  },
+});
+
+const Button = styled("button", buttonStyle);
+
+export default Button;
