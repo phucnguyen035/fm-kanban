6668ef90b9afb99f807d4f7363d09d051fbbd06e
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2024-04-01 22:33:45 +0700

    use tailwind variant to delcare subtask checkbox classes

--- src/components/SubtaskCheckbox.tsx
+++ src/components/SubtaskCheckbox.tsx
@@ -3,25 +3,31 @@
 import * as Checkbox from "@radix-ui/react-checkbox";
 import { CheckIcon } from "@radix-ui/react-icons";
 import { useId } from "react";
+import { tv } from "@/utils/tv";
+
+const subtaskCheckboxClass = tv({
+	base: "flex cursor-pointer items-center gap-x-4 rounded bg-gray-light p-3 transition-colors hover:bg-primary/25 dark:bg-gray-darker",
+	slots: {
+		container:
+			"peer flex size-4 appearance-none items-center justify-center rounded border border-gray-medium/25 bg-white outline-none dark:border-lines-dark dark:bg-gray-dark",
+		indicator: "rounded border border-primary bg-primary text-white",
+		label:
+			"text-bodyM text-black peer-aria-checked:text-opacity-25 peer-aria-checked:line-through dark:text-white",
+	},
+});
+
 export default function SubtaskCheckbox({ label }: { label: string }) {
 	const id = useId();
+	const { base, container, indicator, label: labelClass } = subtaskCheckboxClass();
 
 	return (
-		<label
-			htmlFor={id}
-			className="group flex cursor-pointer items-center gap-x-4 rounded bg-gray-light p-3 transition-colors hover:bg-primary/25 dark:bg-gray-darker"
-		>
-			<Checkbox.Root
-				className="peer flex size-4 appearance-none items-center justify-center rounded border border-gray-medium/25 bg-white outline-none dark:border-lines-dark dark:bg-gray-dark"
-				id={id}
-			>
-				<Checkbox.Indicator className="rounded border border-primary bg-primary text-white">
+		<label htmlFor={id} className={base()}>
+			<Checkbox.Root className={container()} id={id}>
+				<Checkbox.Indicator className={indicator()}>
 					<CheckIcon />
 				</Checkbox.Indicator>
 			</Checkbox.Root>
-			<span className="text-bodyM text-black peer-aria-checked:text-opacity-25 peer-aria-checked:line-through dark:text-white">
-				{label}
-			</span>
+			<span className={labelClass()}>{label}</span>
 		</label>
 	);
 }
