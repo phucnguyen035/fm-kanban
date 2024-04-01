7416ca5ca8663e4746847c474949ebc8776613f8
Phúc Nguyễn <40336551+phucnguyen035@users.noreply.github.com>
2024-04-01 15:10:00 +0700

    use db logger for development

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,6 +2,6 @@ import { sql } from "@vercel/postgres";
 import { drizzle } from "drizzle-orm/vercel-postgres";
 import * as schema from "./schema";
 
-const db = drizzle(sql, { schema });
+const db = drizzle(sql, { schema, logger: process.env.NODE_ENV === "development" });
 
 export { db, schema };
